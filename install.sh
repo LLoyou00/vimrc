dcb8fbf47b1467883375a08c2b063eb24c6efc2c
lloyou00 <lloyou00@163.com>
2017-05-10 18:25:54 +0800

    Fix some bug in install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-!/bin/sh
+#!/bin/sh
+ln -s ~/.vim/.vimrc ../.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Please press the Enter key to confirm installation plugins"
 vim +PluginInstall +qall
-ln -s .vimrc ../.vimrc
